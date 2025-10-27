e3d5edabe17885524abf72df54f28667b00384a1
danhthai2004 <danhthai0304@gmail.com>
2025-10-28 03:35:59 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+  return (
+    <div> My Component </div>
+  );
+}
+
 // arrow function syntax
 const App = () => {
   const [count, setCount] = useState(0)
@@ -18,6 +24,7 @@ const App = () => {
         </a>
       </div>
       <h1>React & Vite</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
