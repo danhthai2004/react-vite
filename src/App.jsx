a3d7960987becf103014424f93a8ae23ac66dc70
danhthai2004 <danhthai0304@gmail.com>
2025-11-02 01:53:47 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,22 @@ import reactLogo from './assets/react.svg';
 // arrow function syntax
 const App = () => {
 
+  const ten = "Thai";
+  const tuoi = 22;
+  const data = {
+    address: "Da Nang",
+    country: "Viet Nam"
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoAdd />
-      <TodoList />
+      <TodoList
+        name={ten}
+        age={tuoi}
+        data={data}
+      />
       <div className='todo-image'>
         <img className='logo' src={reactLogo} />
       </div>
--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,10 +1,20 @@
 import "./todo.css";
 
-const TodoList = () => {
+const TodoList = (props) => { //props là 1 object chứa các thuộc tính được truyền từ component cha
+    // const name = props.name;
+    // const age = props.age;
+    // const data = props.data;
+    const { name, age, data } = props; //destructuring assignment
+
     return (
         <div className="todo-list">
             <div>Learning React</div>
             <div>Learning Spring</div>
+            {/* <div>Name: {props.name}</div> */}
+            <div>Name: {name}</div>
+            <div>Age: {age}</div>
+            <div>Address: {data.address}</div>
+            <div>Country: {data.country}</div>
         </div>
     )
 }
